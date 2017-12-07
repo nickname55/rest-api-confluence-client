2260ee301f7e2b79da17d9d170d681f5634d5c87
AlexanderPampushko <pampushko@gmail.com>
2017-12-07 12:55:51 +0200

    добавил Main класс и зависимости для логирования slf4j

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+
+/**
+ *
+ */
+public class Main
+{
+	private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
+	public static void main(String[] args)
+	{
+		test();
+	}
+	
+	private static void test()
+	{
+		log.error("Hello, error!");
+	}
+	
+}
