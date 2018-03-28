f39c770af1c619d97b90afae9518032ae430aaeb
AlexanderPampushko <pampushko@gmail.com>
2018-03-28 19:41:04 +0300

    Добавил документацию к функция добавления наблюдателей

--- src/main/java/com/pampushko/confluence/rest/Confluence.java
+++ src/main/java/com/pampushko/confluence/rest/Confluence.java
@@ -3316,48 +3316,14 @@ public class Confluence
 	//----------------------------------------------------------------------------------------
 	//начало, Add content watcher
 	
-	//@formatter:off
+	
 	/**
-	 * Функция добавляет пользователя, указанного вами, к списку наблюдателей элемента контента,
+	 * Добавить пользователя, выполняющего запрос, в наблюдатели контента (контент определяется по contentId)
 	 * <br>
-	 * который вы указываете через contentId.
-	 * <br>
-	 * Пользователь не является обязательным параметром.
-	 * <br>
-	 * Если пользователь не указан, то будет использоваться текущий пользователь (отравитель запроса)
-	 * <br>
-	 * В противном случае, вы можете указать пользователя либо с помощью user key
-	 * <br>
-	 * или с помощью имени пользователя (username).
-	 * <br>
-	 * <br>
-	 * <em>Обратите внимание, что только администраторы Confluence могут добавлять в наблюдатели не только себя, но и других пользователей системы.
-	 * </em>
-	 * <br>
-	 * <p>
-	 * <strong>Примеры URI запросов:</strong>
-	 * <ul>
-	 * <li>POST http://example.com/rest/api/user/watch/content/131213</li>
-	 * <li>POST http://example.com/rest/api/user/watch/content/131213?username=jblogs</li>
-	 * <li>POST http://example.com/rest/api/user/watch/content/131213?key=ff8080815a58e24c015a58e263710000</li>
-	 * </ul>
-	 * <p>
-	 * <strong>Дополнительные параметры</strong>
-	 * <ul>
-	 * <li>key (String) -- Optional -- userkey по которому система найдёт пользователя, которого мы хотим добавить наблюдатели</li>
-	 * <li>username (String) -- Optional -- username по которому система найдёт пользователя, которого мы хотим добавить в наблюдатели</li>
-	 * <li>limit (int) -- Optional -- Default: <strong>25</strong> -- сколько элементов из результирующего возвращаемого набора вы хотите получить (после начального индекса, после start)</li>
-	 * </ul>
-	 * <br>
-	 * <h2><strong>Responses:</strong></h2>
-	 * <strong>STATUS 204</strong> -- application/json, такой ответ будет получен, если пользователь успешно добавлен в наблюдатели указанногов вами элементам контента
-	 * <br>
-	 * <strong>STATUS 404</strong> -- такой код будет возвращён, если для укзанного идентификатора контента соответствующий элемент контента не найден.
-	 * <br>
-	 * Или если пользователь выполняющий запрос не имеет достаточных прав доступа для выполнения операции.
-	 * <br>
-	 *
-	 * @return
+	 * <strong>Более подробную документацию можно прочесть в методе : {@link ConfluenceApi#addWatcher(String, Map)}</strong>
+	 * @param contentId идентификатор элемента контента
+	 * @return если операция выполнена успешно то true, в противном случае - false
+	 * @throws IOException
 	 */
 	public boolean addCurrentUserToWatchers(final String contentId)throws IOException
 	{
@@ -3368,6 +3334,20 @@ public class Confluence
 	}
 	//@formatter:on
 	
+	/**
+	 * Добавить пользователя в наблюдатели контента. Пользователя найти по username.
+	 * <br>
+	 * <strong>Более подробную документацию можно прочесть в методе : {@link ConfluenceApi#addWatcher(String, Map)}</strong>
+	 *
+	 * @param contentId
+	 * 		идентификатор элемента контента
+	 * @param username
+	 * 		имя пользователя (login)
+	 *
+	 * @return если операция выполнена успешно то true, в противном случае - false
+	 *
+	 * @throws IOException
+	 */
 	public boolean addWatcherByUsername(final String contentId, final String username) throws IOException
 	{
 		Map<String, String> params = new HashMap<String, String>()
@@ -3380,6 +3360,15 @@ public class Confluence
 		return addUserToWatchers(contentId, params);
 	}
 	
+	/**
+	 * Добавить пользователя в наблюдатели контента. Пользователя найти по userKey.
+	 * <br>
+	 * <strong>Более подробную документацию можно прочесть в методе : {@link ConfluenceApi#addWatcher(String, Map)}</strong>
+	 * @param contentId идентификатор элемента контента
+	 * @param userkey ключ пользователя
+	 * @return если операция выполнена успешно то true, в противном случае - false
+	 * @throws IOException
+	 */
 	public boolean addWatcherByUserKey(final String contentId, final String userkey) throws IOException
 	{
 		Map<String, String> params = new HashMap<String, String>()
@@ -3392,6 +3381,15 @@ public class Confluence
 		return addUserToWatchers(contentId, params);
 	}
 	
+	/**
+	 * Добавляет пользователя к наблюдателям данного контента (по contentId)
+	 * <br>
+	 * <strong>Более подробную документацию можно прочесть в методе : {@link ConfluenceApi#addWatcher(String, Map)}</strong>
+	 * @param contentId идентификатор элемента контента
+	 * @param params отображаение на основе которого генерируют параметры запроса
+	 * @return если операция выполнена успешно то true, в противном случае - false
+	 * @throws IOException
+	 */
 	private boolean addUserToWatchers(final String contentId, Map<String, String> params) throws IOException
 	{
 		Call<Void> addWatcherCall = confluenceApi.addWatcher(contentId, params);
